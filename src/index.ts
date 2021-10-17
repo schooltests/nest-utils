73757abf6729060f3ac0a2de9e2cbe8d36cdc32b
woophi <mik.kon6610@gmail.com>
2021-10-17 20:49:35 +0200

    export

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,11 @@
 export { readMigration } from './db/utils/read-files';
 export { autoRetryTransaction } from './db/utils/transactions';
 export { BusEvent, BusEventDto, EventBus } from './events/events.bus';
+export * from './exceptions';
 export * from './interceptors/exts';
 export { FetchLimiter } from './interceptors/rate-limiter';
 export { TimeoutInterceptor } from './interceptors/timeout.interceptor';
 export { TransformInterceptor } from './interceptors/transform.interceptor';
-export * as redis from './redis-cache/redis-cache.module';
+export { HashKey, HashKeys, HashValue, RedisCacheModule, RedisHashService } from './redis-cache/redis-cache.module';
 export * from './utils';
 export * as vk from './vk';
-export * from './exceptions';
